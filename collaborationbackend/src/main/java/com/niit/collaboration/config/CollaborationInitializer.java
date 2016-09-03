611719c4ea534a87dd2c3d0b0136fa47bf3cfb8c
sanjeev <sanjeevit015@gmail.com>
2016-09-03 11:51:27 +0530

    collaboration first commit

---
+++ collaborationbackend/src/main/java/com/niit/collaboration/config/CollaborationInitializer.java
@@ -0,0 +1,25 @@
+package com.niit.collaboration.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class CollaborationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {CollaborationInitializer.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+
+}
